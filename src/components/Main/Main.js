331bed509daebbbe34bd4c1eeb50f098dc43a015
Nataliya Rezvushkina <rezvushkina@gmail.com>
2021-06-17 15:18:50 +0300

    feat: main: Techs, AboutMe, Portfolio

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,10 @@ import Footer from "../Footer/Footer";
 import  '../../styles/page.css';
 import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
-import AboutProject from "../AboutProject/AboutProject"
+import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
 
 
 function Main(props) {
@@ -14,6 +17,9 @@ function Main(props) {
                     <Promo />
                     <NavTab />
                     <AboutProject />
+                    <Techs />
+                    <AboutMe />
+                    <Portfolio />
                     <Footer/>
                 </main>
             </div>
